69330ff3f521460167be298ff53d48f31034a25a
Marc Berchtold <marc.berchtold@hotmail.de>
2018-07-17 22:01:13 +0200

    New mqtt media message format & display current client state

--- frontend-app/src/app/client/client.component.ts
+++ frontend-app/src/app/client/client.component.ts
@@ -31,8 +31,11 @@ export class ClientComponent implements OnInit {
         if (!this.clients[clients[clientUID].roomname]) {
           this.clients[clients[clientUID].roomname] = [];
         }
-
-        this.clients[clients[clientUID].roomname].push({uid: clientUID, clientname: clients[clientUID].clientname, mediaType: null});
+        if (!this.clients[clients[clientUID].roomname].find(x => x.uid === clientUID)) {
+          this.clients[clients[clientUID].roomname].push({uid: clientUID, clientname: clients[clientUID].clientname, mediaType: null});
+        } else {
+          this.clients[clients[clientUID].roomname].find(x => x.uid === clientUID).clientname = clients[clientUID].clientname;
+        }
       }
     });
     this.roomsService.selectedRoomObservable.subscribe(room => this.selectedRoom = room);
@@ -48,55 +51,44 @@ export class ClientComponent implements OnInit {
     }
 
     switch (true) {
-      case topic.match(/^ar-signage\/.+\/.+\/media\/none$/g) && topic.match(/^ar-signage\/.+\/.+\/media\/none$/g).length > 0:
+      case topic.match(/^ar-signage\/.+\/.+\/media$/g) && topic.match(/^ar-signage\/.+\/.+\/media$/g).length > 0:
         roomname = topic.split(/[\/\/]/g)[1];
         uid = topic.split(/[\/\/]/g)[2];
-        if (!roomname || !uid || !this.clients[roomname]) {
+        if (!roomname || !uid) {
           return;
         }
-        this.clients[roomname].find(x => x.uid === uid).mediaType = 'none';
-        break;
-      case topic.match(/^ar-signage\/.+\/.+\/media\/text$/g) && topic.match(/^ar-signage\/.+\/.+\/media\/text$/g).length > 0:
-        roomname = topic.split(/[\/\/]/g)[1];
-        uid = topic.split(/[\/\/]/g)[2];
-        if (!roomname || !uid || !this.clients[roomname]) {
-          return;
+        if (!this.clients[roomname]) {
+          this.clients[roomname] = [];
         }
-        this.clients[roomname].find(x => x.uid === uid).mediaType = 'text';
-        break;
-      case topic.match(/^ar-signage\/.+\/.+\/media\/image$/g) && topic.match(/^ar-signage\/.+\/.+\/media\/image$/g).length > 0:
-        roomname = topic.split(/[\/\/]/g)[1];
-        uid = topic.split(/[\/\/]/g)[2];
-        if (!roomname || !uid || !this.clients[roomname]) {
-          return;
+        if (!this.clients[roomname].find(x => x.uid === uid)) {
+          this.clients[roomname].push({uid, mediaType: null});
         }
-        this.clients[roomname].find(x => x.uid === uid).mediaType = 'image';
-        break;
-      case topic.match(/^ar-signage\/.+\/.+\/media\/video$/g) && topic.match(/^ar-signage\/.+\/.+\/media\/video$/g).length > 0:
-        roomname = topic.split(/[\/\/]/g)[1];
-        uid = topic.split(/[\/\/]/g)[2];
-        if (!roomname || !uid || !this.clients[roomname]) {
-          return;
-        }
-        this.clients[roomname].find(x => x.uid === uid).mediaType = 'video';
+
+        this.clients[roomname].find(x => x.uid === uid).mediaType = messageObject.value.type;
         break;
     }
   }
 
   public setText(textModal, uid) {
     this.modalService.open(textModal, {ariaLabelledBy: 'modal-basic-title'}).result.then(result => {
-      this.mqttService.mqttModule.mqttClient.publish(`ar-signage/${this.selectedRoom}/${uid}/media/text`, JSON.stringify({
-        value: result
-      }));
+      this.mqttService.mqttModule.mqttClient.publish(`ar-signage/${this.selectedRoom}/${uid}/media`, JSON.stringify({
+        value: {
+          type: 'text',
+          content: result
+        }
+      }), {retain: true});
     }).catch(err => {
       // Closed without submiting
     });
   }
 
   public setTimer(uid) {
-    this.mqttService.mqttModule.mqttClient.publish(`ar-signage/${this.selectedRoom}/${uid}/media/none`, JSON.stringify({
-      value: null
-    }));
+    this.mqttService.mqttModule.mqttClient.publish(`ar-signage/${this.selectedRoom}/${uid}/media`, JSON.stringify({
+      value: {
+        type: 'timer',
+        content: null
+      }
+    }), {retain: true});
   }
 
 }
